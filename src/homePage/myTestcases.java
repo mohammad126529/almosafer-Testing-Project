baa37aeaf5545ddbf80a372d2523d4898ed6e3af
mohammad126529 <mohammadabualrob794@gmail.com>
2024-10-26 15:55:30 +0300

    update

--- src/homePage/myTestcases.java
+++ src/homePage/myTestcases.java
@@ -1,6 +1,9 @@
 package homePage;
 
 import java.time.Duration;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Random;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -16,6 +19,8 @@ public class myTestcases {
 	String Website ="https://global.almosafer.com/en";
 	String ExpectedDefaultLanguage ="en";
 	
+	Random rand = new Random();
+	
 	@BeforeTest
 	public void mySetup() 
 	{
@@ -26,8 +31,7 @@ public class myTestcases {
 	}
 	
 	@Test(priority = 1)
-	public void CheckTheDefaultLanguageIsEnglish() 
-	{
+	public void CheckTheDefaultLanguageIsEnglish() {
 	
 		System.out.println(driver.findElement(By.tagName("html")).getAttribute("lang"));
 		String ActualLanguage = driver.findElement(By.tagName("html")).getAttribute("lang");
@@ -37,6 +41,7 @@ public class myTestcases {
 		
 	@Test(priority = 2)
 	public void CheckDefaultCurrency() {
+		
 		String ExpectedCurrency = "SAR";
 		WebElement Currency = driver.findElement(By.xpath("//button[@data-testid=\"Header__CurrencySelector\"]"));
 		String ActualCurrency = Currency.getText();
@@ -62,6 +67,55 @@ public class myTestcases {
 	boolean ActualResultForTheLogo = theFooter.findElement(By.cssSelector(".sc-ghsgMZ.hIElfs")).findElement(By.cssSelector(".sc-bdVaJa.bxRSiR.sc-ciodno.lkfeIG")).isDisplayed();
 	System.out.println(ActualResultForTheLogo);	
 	Assert.assertEquals(ActualResultForTheLogo, true);
+	
 	}
 	
+	@Test(priority = 5)
+	public void TestHotelTabNotSelected() {
+	
+		String ExpectedHotel = "false";
+		WebElement HotelTab = driver.findElement(By.id("uncontrolled-tab-example-tab-hotels"));
+		String ActualValue =HotelTab.getAttribute("aria-selected");
+		System.out.println(HotelTab.getAttribute("aria-selected"));
+		Assert.assertEquals(ActualValue, ExpectedHotel);
+
+	}
+	
+	@Test(priority = 6)
+	public void CheckDepatureDate() {
+	
+	LocalDate todayDate = LocalDate.now();
+	System.out.println( LocalDate.now().getDayOfMonth());
+	
+	int Today = todayDate.getDayOfMonth();
+	int ExpectedTomorrow = Today +1 ;
+	int ExpectedThedayAfterTomorrow = ExpectedTomorrow +1 ;
+	
+	System.out.println(ExpectedTomorrow );
+	System.out.println(ExpectedThedayAfterTomorrow );
+	
+	
+	List<WebElement> depatureAndArrivalDates = driver.findElements(By.className("hNjEjT"));
+	String ActualDepatureDate = depatureAndArrivalDates.get(0).getText();
+	String ActualFlightReturn = depatureAndArrivalDates.get(1).getText();
+	
+	System.out.println(ActualDepatureDate);
+	System.out.println(ActualFlightReturn);
+	
+	int ActualDepatureDateAsInt = Integer.parseInt(ActualDepatureDate);
+	int ActualFlightReturnAsInt = Integer.parseInt(ActualFlightReturn);
+	
+	Assert.assertEquals(ActualDepatureDateAsInt, ExpectedTomorrow);
+	Assert.assertEquals(ActualFlightReturnAsInt, ExpectedThedayAfterTomorrow);
+	
+	}
+	
+	@Test(priority = 7)
+	public void RandomChangeTheLanguage() {
+		
+		String [] URLs = {"https://www.almosafer.com/en","https://www.almosafer.com/ar"};
+		int RandomURL = rand.nextInt(URLs.length);
+		driver.get(URLs[RandomURL]);
+	}
+
 }
